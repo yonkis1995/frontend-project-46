f5fba7510830c355a57c29adbffd5ada64964b24
Viktor Yevseyev <yonkis1995@gmail.com>
2026-01-31 17:48:48 +0300

    style: fix lint issues

--- src/parseFile.js
+++ src/parseFile.js
@@ -2,24 +2,24 @@ import fs from 'fs'
 import path from 'path'
 
 const readFile = (filepath) => {
-    const absPath = path.resolve(process.cwd(), filepath)
-    const data = fs.readFileSync(absPath, 'utf-8')
-    return data
+  const absPath = path.resolve(process.cwd(), filepath)
+  const data = fs.readFileSync(absPath, 'utf-8')
+  return data
 }
 
 const parse = (data, ext) => {
-    switch (ext) {
-        case '.json':
-            return JSON.parse(data)
-        default:
-            throw new Error('Unsupported file extension')
-    }
+  switch (ext) {
+    case '.json':
+      return JSON.parse(data)
+    default:
+      throw new Error('Unsupported file extension')
+  }
 }
 
 const getData = (filepath) => {
-    const ext = path.extname(filepath)
-    const data = readFile(filepath)
-    return parse(data, ext)
+  const ext = path.extname(filepath)
+  const data = readFile(filepath)
+  return parse(data, ext)
 }
 
 export default getData
