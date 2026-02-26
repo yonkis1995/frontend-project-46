9ba70eb27fe4f3c6321d6e5fa4c21f449ac81c0e
Viktor Yevseyev <yonkis1995@gmail.com>
2026-02-26 23:49:47 +0300

    feat: set stylish as default format in CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,9 @@ program
   .version('0.0.1')
   .argument('<filepath1>', 'path to first file')
   .argument('<filepath2>', 'path to second file')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2)
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    const result = genDiff(filepath1, filepath2, options.format)
     console.log(result)
   })
 
