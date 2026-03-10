796ed2f242156f5335bdd4b4c44645b687a233bc
Viktor Yevseyev <yonkis1995@gmail.com>
2026-03-10 23:28:15 +0300

    add json formatter support

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import stylish from './stylish.js'
 import plain from './plain.js'
+import json from './json.js'
 
 const formatters = {
   stylish,
   plain,
+  json,
 }
 
 export default (tree, formatName) => {
